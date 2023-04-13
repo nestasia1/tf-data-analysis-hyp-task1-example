ca5910eafebc851bd6a5f7db7584a06e2dbe84ac
nestasia1 <109359981+nestasia1@users.noreply.github.com>
2023-04-13 23:33:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 from statsmodels.stats.proportion import proportions_ztest
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1022895883 # Ваш chat ID, не меняйте название переменной
 
 # True if H0 is rejected
 # Interval estimation
-def solutionPropZTest(x_success: int, 
+def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
